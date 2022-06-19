73dc5459acd62f00980b5aa5ff84541e1aa513a1
AndreiCherniaev <dungeonlords789@yandex.ru>
2022-06-20 03:44:10 +0400

    sun is for sun only!
    I don't want sun* for my RK3399

--- scripts/pack-uboot.sh
+++ scripts/pack-uboot.sh
@@ -34,36 +34,41 @@ function do_ini_to_dts()
 
 function do_common()
 {
-	unix2dos sys_config.fex > /dev/null 2>&1
-	script  sys_config.fex  > /dev/null 2>&1
-	cp ${PACKOUT_DIR}/${BOARD}-u-boot.dtb sunxi.fex
-	[[ $BOARDFAMILY == sun50iw2 ]] && update_uboot_fdt u-boot.fex sunxi.fex u-boot.fex >/dev/null
-
-	if [[ $BOARDFAMILY =~ sun50iw6|sun50iw9 ]]; then
-		update_dtb sunxi.fex 4096 >/dev/null
-	fi
+	sun_str='sun'
+	if [[ "$BOARDFAMILY" =~ .*"$sun_str".* ]]; then
 	
-	if [[ $BOARDFAMILY =~ sun50iw6|sun50iw2 ]]; then
-		cp -f sys_config.bin config.fex
-		update_scp scp.fex sunxi.fex > /dev/null 2>&1
-	fi
+		if [[ $BOARDFAMILY =~ sun50iw6|sun50iw9 ]]; then
+		unix2dos sys_config.fex > /dev/null 2>&1
+		script  sys_config.fex  > /dev/null 2>&1
+		cp ${PACKOUT_DIR}/${BOARD}-u-boot.dtb sunxi.fex
+		[[ $BOARDFAMILY == sun50iw2 ]] && update_uboot_fdt u-boot.fex sunxi.fex u-boot.fex >/dev/null
 
-	update_boot0 boot0_sdcard.fex	sys_config.bin SDMMC_CARD > /dev/null
-	if [[ $BOARDFAMILY =~ sun50iw6|sun50iw9 ]]; then
-		update_uboot -no_merge u-boot.fex sys_config.bin > /dev/null
-	elif [[ $BOARDFAMILY =~ sun50iw2 ]]; then
-		update_uboot u-boot.fex sys_config.bin > /dev/null
-	fi
-	[[ $? -ne 0 ]] && exit_with_error "update u-boot run error"
+		if [[ $BOARDFAMILY =~ sun50iw6|sun50iw9 ]]; then
+			update_dtb sunxi.fex 4096 >/dev/null
+		fi
 
-	#pack boot package
-	unix2dos boot_package.cfg > /dev/null 2>&1
-	dragonsecboot -pack boot_package.cfg > /dev/null
-	[[ $? -ne 0 ]] && exit_with_error "dragon pack error"
+		if [[ $BOARDFAMILY =~ sun50iw6|sun50iw2 ]]; then
+			cp -f sys_config.bin config.fex
+			update_scp scp.fex sunxi.fex > /dev/null 2>&1
+		fi
 
-	#Here, will check if need to used multi config.fex or not
-	if [[ $BOARDFAMILY == sun50iw2 ]]; then
-		update_uboot_v2 u-boot.fex sys_config.bin ${CHIP_BOARD} 1>/dev/null 2>&1
+		update_boot0 boot0_sdcard.fex	sys_config.bin SDMMC_CARD > /dev/null
+		if [[ $BOARDFAMILY =~ sun50iw6|sun50iw9 ]]; then
+			update_uboot -no_merge u-boot.fex sys_config.bin > /dev/null
+		elif [[ $BOARDFAMILY =~ sun50iw2 ]]; then
+			update_uboot u-boot.fex sys_config.bin > /dev/null
+		fi
+		[[ $? -ne 0 ]] && exit_with_error "update u-boot run error"
+
+		#pack boot package
+		unix2dos boot_package.cfg > /dev/null 2>&1
+		dragonsecboot -pack boot_package.cfg > /dev/null
+		[[ $? -ne 0 ]] && exit_with_error "dragon pack error"
+
+		#Here, will check if need to used multi config.fex or not
+		if [[ $BOARDFAMILY == sun50iw2 ]]; then
+			update_uboot_v2 u-boot.fex sys_config.bin ${CHIP_BOARD} 1>/dev/null 2>&1
+		fi
 	fi
 }
 

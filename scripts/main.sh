511664887bea87eed7c587cd1f864a702dbc502e
Vlad <anykey196@gmail.com>
2024-02-16 22:32:20 +0500

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -268,6 +268,7 @@ if [[ -z $BRANCH ]]; then
 	[[ $KERNEL_TARGET == *current* ]] && options+=("current" "Recommended. Come with best support")
 	[[ $KERNEL_TARGET == *legacy* ]] && options+=("legacy" "Old stable / Legacy")
 	[[ $KERNEL_TARGET == *next* ]] && options+=("next" "Use the latest kernel")
+ 	[[ $KERNEL_TARGET == *master* ]] && options+=("master" "Linus master git")
 
 	menustr="Select the target kernel branch\nExact kernel versions depend on selected board"
 	# do not display selection dialog if only one kernel branch is available

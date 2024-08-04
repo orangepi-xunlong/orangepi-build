caf0ca3b40dc0e3a7d1b31e1de52543209b7f9af
Vlad <anykey196@gmail.com>
2024-08-04 12:13:41 +0500

    ADD Ubuntu 24.04
    ADD Ubuntu 24.04 noble

--- scripts/general.sh
+++ scripts/general.sh
@@ -1459,7 +1459,7 @@ prepare_host()
 	#
 	# NO_HOST_RELEASE_CHECK overrides the check for a supported host system
 	# Disable host OS check at your own risk. Any issues reported with unsupported releases will be closed without discussion
-	if [[ -z $HOSTRELEASE || "focal jammy" != *"$HOSTRELEASE"* ]]; then
+	if [[ -z $HOSTRELEASE || "focal jammy noble" != *"$HOSTRELEASE"* ]]; then
 		if [[ $NO_HOST_RELEASE_CHECK == yes ]]; then
 			display_alert "You are running on an unsupported system" "${HOSTRELEASE:-(unknown)}" "wrn"
 			display_alert "Do not report any errors, warnings or other issues encountered beyond this point" "" "wrn"
